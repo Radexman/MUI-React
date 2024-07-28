de15f3bbe0d08ebbc980a7ef6a5fa1b0c875b763
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-07-28 13:03:47 +0200

    Switch

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@
 // import MuiTextField from './components/MuiTextField';
 // import MuiSelect from './components/MuiSelect';
 // import MuiRadioButton from './components/MuiRadioButton';
-import MuiCheckbox from './components/MuiCheckbox';
+// import MuiCheckbox from './components/MuiCheckbox';
 import styles from './App.module.css';
+import MuiSwitch from './components/MuiSwitch';
 
 function App() {
 	return (
 		<>
 			<div className={styles.app}>
-				{/* <MuiTypography />
-				<MuiButton />
-				<MuiTextField />
-				<MuiSelect />
-				<MuiRadioButton /> */}
-				<MuiCheckbox />
+				{/* <MuiTypography /> */}
+				{/* <MuiButton /> */}
+				{/* <MuiTextField /> */}
+				{/* <MuiSelect /> */}
+				{/* <MuiRadioButton /> */}
+				{/* <MuiCheckbox /> */}
+				<MuiSwitch />
 			</div>
 		</>
 	);
