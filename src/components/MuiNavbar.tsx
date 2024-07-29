8277d008f9742c4349de8c17777dfe2416beaa8c
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-07-29 19:30:47 +0200

    Menu

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,7 +1,20 @@
-import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material';
+import { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Menu, MenuItem } from '@mui/material';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const MuiNavbar = () => {
+	const [anchoElement, setAnchoElement] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchoElement);
+
+	const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+		setAnchoElement(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchoElement(null);
+	};
+
 	return (
 		<AppBar position='static'>
 			<Toolbar>
@@ -27,8 +40,39 @@ const MuiNavbar = () => {
 					<Button color='inherit'>Features</Button>
 					<Button color='inherit'>Pricing</Button>
 					<Button color='inherit'>About</Button>
+					<Button
+						color='inherit'
+						id='resources-button'
+						onClick={handleClick}
+						aria-controls={open ? 'resources-menu' : undefined}
+						aria-haspopup='true'
+						aria-expanded={open ? 'true' : undefined}
+						endIcon={<KeyboardArrowDownIcon />}
+					>
+						Resources
+					</Button>
 					<Button color='inherit'>Login</Button>
 				</Stack>
+				<Menu
+					id='resources-menu'
+					anchorEl={anchoElement}
+					open={open}
+					MenuListProps={{
+						'aria-labelledby': 'resouces-button',
+					}}
+					onClose={handleClose}
+					anchorOrigin={{
+						vertical: 'bottom',
+						horizontal: 'right',
+					}}
+					transformOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+				>
+					<MenuItem onClick={handleClose}>Blog</MenuItem>
+					<MenuItem onClick={handleClose}>Podcast</MenuItem>
+				</Menu>
 			</Toolbar>
 		</AppBar>
 	);
