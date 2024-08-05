90142c3477cf95a88004c6506f6343d451240135
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-08-05 17:23:40 +0200

    Chip

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,8 @@
 // import MuiBottomNavigation from './components/MuiBottomNavigation';
 // import MuiAvatar from './components/MuiAvatar';
 // import MuiBadge from './components/MuiBadge';
-import MuiList from './components/MuiList';
+// import MuiList from './components/MuiList';
+import MuiChip from './components/MuiChip';
 // import styles from './App.module.css';
 
 function App() {
@@ -47,7 +48,8 @@ function App() {
 				{/* <MuiLink /> */}
 				{/* <MuiAvatar /> */}
 				{/* <MuiBadge /> */}
-				<MuiList />
+				{/* <MuiList /> */}
+				<MuiChip />
 			</div>
 		</>
 	);
